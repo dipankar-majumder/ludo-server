92e5bd4c9bb58b029a649cd9507000bbdd58a76b
Dipankar Majumder <dipankarmajumder2000@outlook.com>
2020-05-08 19:20:09 +0530

    dev: new & join game

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import socketIO = require('socket.io');
 import { Application, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
+console.clear();
 console.log('Server Execution Started.');
 
 const app: Application = express();
@@ -22,16 +23,28 @@ io.on('connection', socket => {
     }`,
   );
 
-  socket.on('set_nick_name', nickName => {
-    console.log(nickName);
-  });
   // https://stackoverflow.com/questions/19150220/creating-rooms-in-socket-io
-  socket.on('new_game', (roomName = uuidv4(), callback: Function) => {
-    socket.join(roomName);
-    callback(roomName);
-
+  socket.on('new_game', callback => {
+    const room = uuidv4();
+    socket.join(room, err => {
+      if (err) {
+        console.log(`new_game => Error: ${err}`);
+        callback(err);
+      } else {
+        console.log(`Room Created\t${room}`);
+      }
+    });
+    callback(room);
     // socket.in(room).emit('commands', data);
   });
+
+  socket.on('join_game', (roomName: string, callback: Function) => {
+    socket.join(roomName, err => {
+      console.log('join_game => Error: ', err);
+      callback(err);
+    });
+  });
+
   socket.on('disconnect', () => {
     console.log(
       `User disconnected\t${socket.id}\tTotal Connected Clients: ${
