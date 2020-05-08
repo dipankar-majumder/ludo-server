ff76f12956a4cd52d011b8316e1055da923517ab
Dipankar Majumder <dipankarmajumder2000@outlook.com>
2020-05-08 15:07:25 +0530

    improve log

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express = require('express');
 import socketIO = require('socket.io');
 import { Application, Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 console.log('Server Execution Started.');
 
@@ -15,12 +16,32 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 io.on('connection', socket => {
-  console.log(`User connected ${socket.id}`);
+  console.log(
+    `User   connected\t${socket.id}\tTotal Connected Clients: ${
+      Object.keys(io.sockets.sockets).length
+    }`,
+  );
+
+  socket.on('set_nick_name', nickName => {
+    console.log(nickName);
+  });
   // https://stackoverflow.com/questions/19150220/creating-rooms-in-socket-io
-  socket.on('create_room', room => {
-    socket.join(room);
+  socket.on('new_game', (roomName = uuidv4(), callback: Function) => {
+    socket.join(roomName);
+    callback(roomName);
+
     // socket.in(room).emit('commands', data);
+  });
+  socket.on('disconnect', () => {
+    console.log(
+      `User disconnected\t${socket.id}\tTotal Connected Clients: ${
+        Object.keys(io.sockets.sockets).length
+      }`,
+    );
   });
 });
 
-http.listen(process.env.PORT || 5000);
+const port = process.env.PORT || 5000;
+http.listen(port, () =>
+  console.log(`Server Started at http://localhost:${port}`),
+);
